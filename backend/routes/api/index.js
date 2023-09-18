38fad98d03e5e549467c4a9eafa73f70d17c8cc3
AnthonyFebles <AnthonyFebles21@outlook.com>
2023-09-18 16:49:13 -0400

    phase 1 partially done

---
+++ backend/routes/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+
+
+//test api router
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
+
+
+ 
+
+module.exports = router 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const apiRouter = require('./api')
 
 //XSRF-TOKEN cookie
 router.get('/hello/world', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken())
     res.send('Hello World!')
 })
+
+router.use('/api', apiRouter)
 
 module.exports = router
