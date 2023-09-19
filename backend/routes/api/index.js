f75e9f72c50dfb6d1bd7b7c261bb95f41058772b
AnthonyFebles <anthonyfebles21@outlook.com>
2023-09-18 20:28:32 -0400

    schema name

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const { setTokenCookie } = require("../../utils/auth.js");
 const { User } = require("../../db/models");
 const { restoreUser } = require("../../utils/auth.js");
+const { requireAuth } = require("../../utils/auth.js");
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
@@ -29,7 +30,6 @@ router.get("/restore-user", (req, res) => {
 });
 
 //test require auth middleware 
-const { requireAuth } = require("../../utils/auth.js");
 router.get("/require-auth", requireAuth, (req, res) => {
 	return res.json(req.user);
 });
