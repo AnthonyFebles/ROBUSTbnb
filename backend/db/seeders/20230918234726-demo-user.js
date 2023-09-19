c763b6c74cde91fe74356fb7cd87f26d22e9c581
AnthonyFebles <anthonyfebles21@outlook.com>
2023-09-18 20:05:09 -0400

    added user model flow

--- backend/db/seeders/20230918234726-demo-user.js
+++ backend/db/seeders/20230918234726-demo-user.js
@@ -14,18 +14,18 @@ module.exports = {
 			[
 				{
 					email: "demo@user.io",
-					username: "Demo-lition",
-					hashedPassword: bcrypt.hashSync("password"),
+					username: "Isa-Demo",
+					hashedPassword: bcrypt.hashSync("password123"),
 				},
 				{
 					email: "user1@user.io",
 					username: "FakeUser1",
-					hashedPassword: bcrypt.hashSync("password2"),
+					hashedPassword: bcrypt.hashSync("hunter2"),
 				},
 				{
 					email: "user2@user.io",
 					username: "FakeUser2",
-					hashedPassword: bcrypt.hashSync("password3"),
+					hashedPassword: bcrypt.hashSync("supercalifragilistic"),
 				},
 			],
 			{ validate: true }
