cda4fd27313d70b5250e94c1796c1bb24c6527d2
AnthonyFebles <anthonyfebles21@outlook.com>
2023-09-20 02:45:35 -0400

    seperated first and last name migration files

--- backend/db/migrations/20230920032108-add-first-name-and-lastname-to-user.js
+++ backend/db/migrations/20230920032108-add-first-name-and-lastname-to-user.js
@@ -1,42 +1,16 @@
-'use strict';
-let options = {}
+let options = {};
 if (process.env.NODE_ENV === "production") {
 	options.schema = process.env.SCHEMA; // define your schema in options object
 }
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-    await queryInterface.addColumn(options, "Users", "firstName", {
-	
-				type: Sequelize.STRING(30),
-				allowNull: false,
-			
-		}, );
-
-    await queryInterface.addColumn(options, "Users", "lastName", {
-
+	async up(queryInterface, Sequelize) {
+		options.tableName = "Users";
+		await queryInterface.addColumn(options, "firstName", {
 			type: Sequelize.STRING(30),
-			allowNull: false,
-
-		}, );
-  },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-
-    await queryInterface.removeColumn(options, "Users", "firstName")
-    await queryInterface.removeColumn(options,"Users", "lastName")
-  }
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		options.tableName = "Users";
+		await queryInterface.removeColumn(options, "firstName");
+	},
 };
