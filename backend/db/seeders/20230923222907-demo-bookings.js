8f39e7d7ab717c97c2c4d1399438a6d9af19f14f
Anthony Febles <anthonyfebles21@outlook.com>
2023-10-03 21:18:02 -0400

    changed bulkcreate for bulkinsert in hopes to fix seeders in deployment

--- backend/db/seeders/20230923222907-demo-bookings.js
+++ backend/db/seeders/20230923222907-demo-bookings.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
 	async up(queryInterface, Sequelize) {
     options.tableName = "Bookings";
-		await Booking.bulkCreate(options,
+		return queryInterface.bulkInsert(options,
 			[
 				{
 					userId: 1,
