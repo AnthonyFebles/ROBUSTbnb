aa115c58b0b5bb8b8856b0597153b79cb1285238
Anthony Febles <anthonyfebles21@outlook.com>
2023-11-06 11:12:47 -0500

    changed page title and added icon

---
+++ frontend/src/components/AllSpots/index.js
@@ -0,0 +1,15 @@
+//aka homepage
+
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import "./AllSpots.css";
+
+
+const AllSpots = () => { 
+
+    
+}
+
+
+export default AllSpots
