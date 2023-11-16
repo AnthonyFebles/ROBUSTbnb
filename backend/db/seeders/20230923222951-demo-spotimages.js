ff562a28f75de6157376bf29ec2c60e7ae049f77
AnthonyFebles <anthonyfebles21@outlook.com>
2023-11-16 14:29:04 -0500

    changing some spot speeders to have actual image urls and also just different more interesting spots in general

--- backend/db/seeders/20230923222951-demo-spotimages.js
+++ backend/db/seeders/20230923222951-demo-spotimages.js
@@ -12,34 +12,104 @@ module.exports = {
 		options.tableName = "SpotImages";
 		return queryInterface.bulkInsert(options, [
 			{
-				url: "https://tinypic.host/images/2023/11/06/App_Academy_Building.jpeg",
+				url: "https://tinypic.host/images/2023/11/16/preview.jpeg",
 				spotId: 1,
 				isPreview: true,
 			},
 			{
-				url: "images.url",
+				url: "https://tinypic.host/images/2023/11/16/hokage_residence_mansion_by_masterchristian_df82wui-pre.jpeg",
 				spotId: 1,
 				isPreview: false,
 			},
 			{
-				url: "images.urls",
+				url: "https://tinypic.host/images/2023/11/16/preview2.jpeg",
+				spotId: 1,
+				isPreview: false,
+			},
+			{
+				url: "hhttps://tinypic.host/images/2023/11/16/preview3.jpeg",
+				spotId: 1,
+				isPreview: false,
+			},
+			{
+				url: "hhttps://tinypic.host/images/2023/11/16/preview3.jpeg",
 				spotId: 1,
 				isPreview: false,
 			},
 			{
 				url: "https://i.imgur.com/f9iJGg7.jpg",
-				spotId: 2,
+				spotId: 3,
 				isPreview: true,
 			},
 			{
 				url: "https://tinypic.host/images/2023/11/06/Capsule_Corp.webp",
 				spotId: 3,
+				isPreview: false,
+			},
+			{
+				url: "https://tinypic.host/images/2023/11/16/preview3e03fe4eaa673bc7a.jpeg",
+				spotId: 3,
+				isPreview: false,
+			},
+			{
+				url: "https://tinypic.host/images/2023/11/16/preview2.png",
+				spotId: 3,
+				isPreview: false,
+			},
+			{
+				url: "https://tinypic.host/images/2023/11/16/preview4.jpeg",
+				spotId: 3,
+				isPreview: false,
+			},
+			{
+				url: "https://tinypic.host/images/2023/11/16/cabin-in-the-woods.jpeg",
+				spotId: 2,
 				isPreview: true,
+			},
+			{
+				url: "https://tinypic.host/images/2023/11/16/preview1.png",
+				spotId: 2,
+				isPreview: false,
+			},
+			{
+				url: "https://tinypic.host/images/2023/11/16/preview296c8459085143518.png",
+				spotId: 2,
+				isPreview: false,
+			},
+			{
+				url: "https://tinypic.host/images/2023/11/16/preview3.png",
+				spotId: 2,
+				isPreview: false,
+			},
+			{
+				url: "https://tinypic.host/images/2023/11/16/preview47679ce588360813f.jpeg",
+				spotId: 2,
+				isPreview: false,
 			},
 			{
 				url: "https://tinypic.host/images/2023/11/06/Capsule_Corporation.webp",
 				spotId: 4,
 				isPreview: true,
+			},
+			{
+				url: "https://tinypic.host/images/2023/11/06/Capsule_Corp.webp",
+				spotId: 4,
+				isPreview: false,
+			},
+			{
+				url: "https://tinypic.host/images/2023/11/06/Capsule_Corp.webp",
+				spotId: 4,
+				isPreview: false,
+			},
+			{
+				url: "https://tinypic.host/images/2023/11/06/Capsule_Corp.webp",
+				spotId: 4,
+				isPreview: false,
+			},
+			{
+				url: "https://tinypic.host/images/2023/11/06/Capsule_Corp.webp",
+				spotId: 4,
+				isPreview: false,
 			},
 		]);
 	},
